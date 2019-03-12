af545f2f5c9ac94b3712741efbcb43ccc2fc551f
Daniel dos Santos <dd_santos@id.uff.br>
2019-03-12 11:30:52 -0300

    added linux option

--- torrent_download.py
+++ torrent_download.py
@@ -8,7 +8,12 @@ def torrent_download(message):
     mirror = re.compile(r'/torrent (magnet:.*)')
 
     if mirror.search(message):
-        # Open qbittorrent
+        # Open qbittorrent windows
         subprocess.Popen([r'C:\Users\Danie\\AppData\Roaming\BitTorrent\BitTorrent.exe', mirror.search(message).group(1)])
-    
+        # Open transmission linux
+        try:
+            subprocess.Popen([r'transmission-gtk', mirror.search(message).group(1)])
+        
+        # TODO: Create my own torrent downloader so I don't need to use a subprocess
+        
     time.sleep(1)
