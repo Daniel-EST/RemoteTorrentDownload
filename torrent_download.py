2261d8af87068b3ed5f853ed2e868e5bbd0ee509
Daniel-EST <user@exemplo.com>
2019-03-12 11:47:34 -0300

    added download in linux linux

--- torrent_download.py
+++ torrent_download.py
@@ -8,12 +8,18 @@ def torrent_download(message):
     mirror = re.compile(r'/torrent (magnet:.*)')
 
     if mirror.search(message):
-        # Open qbittorrent windows
-        subprocess.Popen([r'C:\Users\Danie\\AppData\Roaming\BitTorrent\BitTorrent.exe', mirror.search(message).group(1)])
-        # Open transmission linux
         try:
-            subprocess.Popen([r'transmission-gtk', mirror.search(message).group(1)])
+            # Open qbittorrent windows
+            subprocess.Popen([r'C:\Users\Danie\\AppData\Roaming\BitTorrent\BitTorrent.exe', mirror.search(message).group(1)])
+        except FileNotFoundError:
+            pass
         
+        try:
+            # Open transmission linux
+            subprocess.Popen([r'transmission-gtk', mirror.search(message).group(1)])
+        except FileNotFoundError:
+            pass
+            
         # TODO: Create my own torrent downloader so I don't need to use a subprocess
         
     time.sleep(1)
